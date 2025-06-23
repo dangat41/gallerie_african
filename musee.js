971e7479d6691d204e1e986d1e9256fb2f564611
dangat41 <gatembodan04@gmail.com>
2025-06-23 19:44:53 +0100

    Add files via upload

--- musee.js
+++ musee.js
@@ -5,6 +5,7 @@ menu.addEventListener("click", function() {
     listes.style.display = "flex";
 })
 
+
 window.addEventListener('scroll', function(){
     const header =this.document.querySelector('header');
     header.classList.toggle("sticky", this.window.scrollY > 0 );
